34951b0f48dd3fbdfbcb396c1b2cc8c19bf18221
Ping <88713384+Ineertiax@users.noreply.github.com>
2022-04-02 15:25:08 +0700

    SimpleFan

--- SimpleFan.ino
+++ SimpleFan.ino
@@ -1,8 +1,8 @@
-//Define connected pin using PB8 and PB9
+// Define connected pin using PB8 and PB9
 #define INA = PB8;
 #define INB = PB9;
 
-//Setup digital pin INA and INB as an output
+// Setup digital pin INA and INB as an output
 void setup() 
 { 
   pinMode(INA,OUTPUT); 
@@ -11,23 +11,23 @@ void setup()
 
 void loop() 
 { 
- // LEFT
+ // Forward
   digitalWrite(INA,LOW);
   digitalWrite(INB,HIGH); 
-  delay(2000);              //wait for 2 second
+  delay(2000);              // wait for 2 second
   
-  // STOP
+  // Stop
   digitalWrite(INA,LOW);
   digitalWrite(INB,LOW); 
-  delay(5000);             //wait for 5 second
+  delay(5000);             // wait for 5 second
 
-  // RIGHT
+  // Backward
   digitalWrite(INA,HIGH);
   digitalWrite(INB,LOW); 
-  delay(2000);            //wait for 2 second
+  delay(2000);            // wait for 2 second
 
-  // STOP
+  // Stop
   digitalWrite(INA,LOW);
   digitalWrite(INB,LOW); 
-  delay(5000);            //wait for 5 second
+  delay(5000);            // wait for 5 second
 }
