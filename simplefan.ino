4ce0217370bcce21a777fcceab0c494507f86398
Ping <88713384+Ineertiax@users.noreply.github.com>
2022-04-02 15:16:17 +0700

    Simple Fan Test

---
+++ simplefan.ino
@@ -0,0 +1,35 @@
+//Define connected pin
+#define INA = PB8;
+#define INB = PB9;
+
+//Setup digital pin INA and INB as an output
+void setup() 
+{ 
+  pinMode(INA,OUTPUT); 
+  pinMode(INB,OUTPUT); 
+} 
+
+void loop() 
+{ 
+
+ // LEFT
+  digitalWrite(INA,LOW);
+  digitalWrite(INB,HIGH); 
+  delay(2000);              //wait for 2 second
+  
+  // STOP
+  digitalWrite(INA,LOW);
+  digitalWrite(INB,LOW); 
+  delay(5000);             //wait for 5 second
+
+  // RIGHT
+  digitalWrite(INA,HIGH);
+  digitalWrite(INB,LOW); 
+  delay(2000);            //wait for 2 second
+
+  // STOP
+  digitalWrite(INA,LOW);
+  digitalWrite(INB,LOW); 
+  delay(5000);            //wait for 5 second
+
+}
