47ced84b3415748c6f28196d0b2828c12654ed4e
Ping <88713384+Ineertiax@users.noreply.github.com>
2022-04-06 16:13:29 +0700

    SimpleTouch

--- simpletouch.ino
+++ simpletouch.ino
@@ -1,13 +1,11 @@
 #include <Wire.h>  
-// Define connected pin using PB7 and using led on stm32 PC13
+// Define connected pin using PB7 
 int touchpin = PB7;
-int ledpin = PC13; 
 
-// Setup digital pin touchpin as input and ledpin as an output
+// Setup digital pin touchpin as input
 void setup() 
 { 
   Serial.begin(9600); // Showing result in serial monitor with speed as 9600
-  pinMode(ledpin, OUTPUT);
   pinMode(touchpin, INPUT);
 }
 
@@ -15,9 +13,6 @@ void loop() {
   int touchval = digitalRead(touchpin);
   if (touchval == HIGH) {
     Serial.println("Touched"); // Showing "Touched" on serial monitor when touchval is HIGH signal
-    digitalWrite(ledpin, HIGH); // Led on stm32 will blinking when touch sensor detected
-  } else {
-    digitalWrite(ledpin, LOW); // If touch senser not detected led on stm32 off
-  }
-  delay(1000); // Waits for 1 second
+  } 
+  delay(500); // Waits for 0.5 second
 }
