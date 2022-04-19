b32d34d552e273e3fc727d9db0f8d9ae7489b9f1
Akira <akira.te@ku.th>
2022-04-19 17:38:11 +0700

    added comments

--- LDRSensor/LDR_On-OffLED/LDR_On-OffLED.ino
+++ LDRSensor/LDR_On-OffLED/LDR_On-OffLED.ino
@@ -1,6 +1,6 @@
 //#include <MapleFreeRTOS821.h>
-#define LDR_PIN 0
-#define LED_PIN 17
+#define LDR_PIN 0   //LDR pin on PA0
+#define LED_PIN 17  //LED pin on PB1
 
 
 void setup() {
@@ -13,13 +13,17 @@ void setup() {
 void loop() {
   int ldrStatus = analogRead(LDR_PIN);
 
+  //Lights are on
   if (ldrStatus <= 1000) {
 
     digitalWrite(LED_PIN, LOW);
     Serial.print("Its BRIGHT, Turn off the LED : ");
     Serial.println(ldrStatus);
 
-  } else {
+
+  }
+  //Lights are off
+  else {
 
     digitalWrite(LED_PIN, HIGH);
     Serial.print("Its DARK, Turn on the LED : ");
