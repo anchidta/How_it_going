5a0f8c5c3ac0c327aea5b788c889149c13e9cc4b
KawinpopChumchuensuk <102815233+KawinpopChumchuensuk@users.noreply.github.com>
2022-04-22 14:02:50 +0700

    Add files via upload

---
+++ DHTSensor/Current_dht.ino
@@ -0,0 +1,147 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+
+#define DHTPIN PA1     // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+int sensorTA12 = PA2; // Analog input pin that sensor is attached to
+
+float nVPP;   // Voltage measured across resistor
+float nCurrThruResistorPP; // Peak Current Measured Through Resistor
+float nCurrThruResistorRMS; // RMS current through Resistor
+float nCurrentThruWire;     // Actual RMS current in Wire
+
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+   nVPP = getVPP();
+   
+   /*
+   Use Ohms law to calculate current across resistor
+   and express in mA 
+   */
+   
+   nCurrThruResistorPP = (nVPP/200.0) * 1000.0;
+   
+   /* 
+   Use Formula for SINE wave to convert
+   to RMS 
+   */
+   
+   nCurrThruResistorRMS = nCurrThruResistorPP * 0.707;
+   
+   /* 
+   Current Transformer Ratio is 1000:1...
+   
+   Therefore current through 200 ohm resistor
+   is multiplied by 1000 to get input current
+   */
+   
+   nCurrentThruWire = nCurrThruResistorRMS * 1000;
+
+   
+   Serial.print("Volts Peak : ");
+   Serial.println(nVPP,3);
+ 
+   
+   Serial.print("Current Through Resistor (Peak) : ");
+   Serial.print(nCurrThruResistorPP,3);
+   Serial.println(" mA Peak to Peak");
+   
+   Serial.print("Current Through Resistor (RMS) : ");
+   Serial.print(nCurrThruResistorRMS,3);
+   Serial.println(" mA RMS");
+   
+   Serial.print("Current Through Wire : ");
+   Serial.print(nCurrentThruWire,3);
+   Serial.println(" mA RMS");
+   
+   Serial.println();
+   
+}
+float getVPP()
+{
+  float result;
+  int readValue;             //value read from the sensor
+  int maxValue = 0;          // store max value here
+   uint32_t start_time = millis();
+   while((millis()-start_time) < 1000) //sample for 1 Sec
+   {
+       readValue = analogRead(sensorTA12);
+       // see if you have a new maxValue
+       if (readValue > maxValue) 
+       {
+           /*record the maximum sensor value*/
+           maxValue = readValue;
+       }
+   }
+   
+   // Convert the digital data to a voltage
+   result = (maxValue * 5.0)/1024.0;
+  
+   return result;
+ }
